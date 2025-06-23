3be7a9ed8539e823b271a20fb93b17770dc3e8e1
Daniel Ganem <daniel.ganem@icloud.com>
2025-06-23 17:08:27 +0200

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by daniel ganem on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VitesseTests/VitesseTests.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseTests.swift
+//  VitesseTests
+//
+//  Created by daniel ganem on 23/06/2025.
+//
+
+import Testing
+@testable import Vitesse
+
+struct VitesseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
