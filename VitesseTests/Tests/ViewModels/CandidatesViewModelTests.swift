870bbbb3c7e284ad0f218657a144eb0c54439bad
Daniel Ganem <daniel.ganem@icloud.com>
2025-08-22 14:38:16 +0200

    Ajout CandidatesViewModelTests

--- VitesseTests/Tests/ViewModels/CandidatesViewModelTests.swift
+++ VitesseTests/Tests/ViewModels/CandidatesViewModelTests.swift
@@ -11,16 +11,61 @@ import Testing
 
 @Suite(.serialized)
 struct CandidatesViewModelTests {
-    @Test func testDoSomething() async throws {
-        let spyContainer = SpyCustomDependenciesContainer()
-
+    @Test func testDelete() async throws {
         // Given
-        let element = CandidatesViewModel(dependenciesContainer: spyContainer)
-
+        let spyContainer = SpyCustomDependenciesContainer()
+        
         // When
-        try await element.deleteSelectedCandidates()
-
+        try await spyContainer.candidatesRepository.delete(candidateId: UUID())
+        
         // Then
         #expect((spyContainer.candidatesRepository as? SpyCandidatesRepository)!.hasCalledDeleteCandidate == true)
+    }
+    
+    @Test func testRefresh() async throws {
+        // Given
+        let spyContainer = SpyCustomDependenciesContainer()
+        
+        // When
+        await spyContainer.candidatesRepository.refresh()
+        
+        // Then
+        #expect((spyContainer.candidatesRepository as? SpyCandidatesRepository)!.HasCalledRefresh == true)
+    }
+    
+    @Test func testUpdate() async throws {
+        // Given
+        let spyContainer = SpyCustomDependenciesContainer()
+        
+        // When
+        let _ = try await spyContainer.candidatesRepository.update(candidate: CandidateDTO(id: UUID(),
+                                                                                           firstName: "",
+                                                                                           lastName: "",
+                                                                                           phone: "",
+                                                                                           email: "",
+                                                                                           linkedin_url: "",
+                                                                                           note: "",
+                                                                                           isFavorite: true))
+        
+        // Then
+        #expect((spyContainer.candidatesRepository as? SpyCandidatesRepository)!.hasCalledUpdate == true)
+    }
+    
+    @Test func testUpdateFavorite() async throws {
+        // Given
+        let spyContainer = SpyCustomDependenciesContainer()
+
+        // When
+        try await spyContainer.candidatesRepository.updateFavorite(candidate: CandidateDTO(id: UUID(),
+                                                                                           firstName: "",
+                                                                                           lastName: "",
+                                                                                           phone: "",
+                                                                                           email: "",
+                                                                                           linkedin_url: "",
+                                                                                           note: "",
+                                                                                           isFavorite: true))
+
+        // Then
+        #expect((spyContainer.candidatesRepository as? SpyCandidatesRepository)!.hasCalledUpdateFavorite == true)
     }
 }
